eb98a9e6e9629918cfeca5340feee69aff9b4299
tnivyj <fishell.yu@qq.com>
2018-04-11 15:58:51 +0800

    第四次練習(Intent)

--- app/src/main/java/com/example/cjcu/bmi/MainActivity.java
+++ app/src/main/java/com/example/cjcu/bmi/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cjcu.bmi;
 
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -43,5 +44,8 @@ public class MainActivity extends AppCompatActivity {
                 .setMessage("BMI="+bmi)
                 .setTitle("BMI值")
                 .show();
+
+        Intent intent = new Intent(this, ResultActivity.class);
+        startActivity(intent);
     }
 }
